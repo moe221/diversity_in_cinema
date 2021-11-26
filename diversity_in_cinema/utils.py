5ea05da4fb5d65357412e873d798eeb5074576cc
Mohamed Abuhalala <myf.abuhalala@gmail.com>
2021-11-26 10:15:56 +0100

    return extracted faces as numpy arrays

--- diversity_in_cinema/utils.py
+++ diversity_in_cinema/utils.py
@@ -1,5 +1,6 @@
 from facenet_pytorch import MTCNN, extract_face
 from PIL import Image
+import numpy as np
 
 
 def extract_face_mtcnn(image):
@@ -28,7 +29,11 @@ def extract_face_mtcnn(image):
             if prob >= 0.99:
 
                 face = extract_face(img, box)
-                face_list.append(face)
+
+                out = np.array(face.permute(1, 2, 0))
+                out = out.astype('uint8')
+
+                face_list.append(out)
 
     print(f"{len(face_list)} faces detected.")
 
